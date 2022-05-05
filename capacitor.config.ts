b1c08228c6cf15505f446170a36be504eaa54d3b
MelvolaMonique <moniqueevelyn600@gmail.com>
2022-05-05 09:06:45 -0300

    First commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sorveteriaRulesApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
